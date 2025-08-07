74a2fee4103375eabbcb03ce18258ca7467a05fe
full-stack-website-developer <talhasajjad148@gmail.com>
2025-08-07 21:01:13 +0500

    base-setup

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,18 @@
+import { Header } from '@/components/layout/header/main-header';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '../styles/globals.scss';
+
+
+export const metadata = {
+  title: 'Wedding Barn',
+  description: 'Wedding venue website',
+};
+
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <body>
+        <Header />
         {children}
       </body>
     </html>
