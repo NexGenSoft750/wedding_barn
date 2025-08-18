68fba34c735557bdc51eafb9525c421d1297d203
full-stack-website-developer <talhasajjad148@gmail.com>
2025-08-18 11:05:19 +0500

    some new sections created on Home Page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,20 @@
 import Carousel from "@/components/common/Caraousel";
-import LookAround from "@/components/LookAround";
+import LookAround from "@/components/home/look-around/LookAround";
+import HomeVenueSection from "@/components/home/venue-section/HomeVenueSection";
 
 export default function Home() {
   const slides = [
-    'https://c4.wallpaperflare.com/wallpaper/26/26/373/full-size-hd-nature-1920x1200-wallpaper-preview.jpg',
-    'https://static.showit.co/327/sdJOL-vVSNuHg6oH70u7Pw/105740/bobby_and_ariel_-_march_2022-387_websize.jpg',
-    'https://static.showit.co/327/sdJOL-vVSNuHg6oH70u7Pw/105740/bobby_and_ariel_-_march_2022-387_websize.jpg',
+    'https://www.villacenci.it/wp-content/uploads/2024/04/214801503_352711262878790_1480070040761242172_n.jpg',
+    'https://owenhouseweddingbarn.co.uk/wp-content/uploads/2023/10/18077025_1991161167766668_6391971413047393111_o.webp',
+    'https://images.squarespace-cdn.com/content/v1/673cdd7b13abb72871e617c8/642dcd80-030b-49cf-b079-ea8c59228457/AndreaPaul_0006.jpg',
+    'https://images.squarespace-cdn.com/content/v1/673cdd7b13abb72871e617c8/1ef9588f-64a4-4aa0-a3fe-8f58fbc624a9/AndreaPaul_0922.jpg?format=2500w',
   ];
 
   return (
     <main>
       <Carousel slides={slides}/>
       <LookAround />
+      <HomeVenueSection />
     </main>
   );
 }
---
+++ src/components/home/venue-section/WhyChooseUs.jsx
@@ -0,0 +1,35 @@
+import styles from "./WhyChooseUs.module.scss";
+
+function WhyChooseUs() {
+    return (
+        <div class={`${styles.main}`}>
+            <h3>Why Choose Acre 31?</h3>
+            <div className={`${styles.content}`}>
+                <div className={`${styles['list-items']}`}>
+                    <ul>
+                        <li>
+                            <b>Affordable Luxury:</b> Enjoy the charm and sophistication of a high-end venue at a fraction of the cost. Our competitive pricing ensures you get the best value for your investment.
+                        </li>
+                        <li>
+                            <b>Stunning Views:</b> Capture the magic of your event with breathtaking hilltop vistas, serene pond views, and mature Spanish oaks and pine trees scattered throughout our 31-acre property, providing a stunning backdrop for your photos.
+                        </li>
+                        <li>
+                            <b>Elegant Spaces:</b> Our venue features a 2400 sq foot event barn (accommodates up to 150 people), a 2400 sq foot spacious patio with an outdoor ceremony area, and a graveled cocktail area. These offer versatile settings that can accommodate events of all sizes and styles.
+                        </li>
+                        <li>
+                            <b>Exceptional Service:</b> Our dedicated team is committed to making your wedding day flawless, ensuring every detail of your wedding is perfect from start to finish.
+                        </li>
+                        <li>
+                            <b>Convenient Location:</b> Located just 50 minutes from Austin, Acre 31 is easily accessible for you and your guests. It is 1 hour 40 mins from Houston and 2 hours from San Antonio.
+                        </li>
+                    </ul>
+                </div>
+                <div className={`${styles['image']}`}>
+                    <img src="https://acre31.com/Venue_Couple_at_arch.png" alt="" />
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default WhyChooseUs;
